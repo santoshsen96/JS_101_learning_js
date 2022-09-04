4c966d620cf6ec071c24ac6b1e4b4c3758ebc11c
santosh kumar sen <santosh7381070816@gmail.com>
2022-09-04 18:36:01 +0530

    mathematical operation

---
+++ thursday-assign/problem2.js
@@ -0,0 +1,12 @@
+let one = 1*2;
+
+let two = 2*2;
+
+let three = 3*2;
+
+let four = 4*2;
+
+let five = 5*2;
+
+let sum = (one+two+three+four+five);
+console.log(sum);
---
+++ thursday-assign/problem3.js
@@ -0,0 +1,15 @@
+
+let one = 1;
+
+let two = 2*2;
+
+let three = 3*3;
+
+let four = 4*3;
+
+let five = 5*2;
+
+let six = 6*2;
+
+let sum = (one+two+three+four+five+six);
+console.log(sum);
